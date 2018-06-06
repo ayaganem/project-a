f9aade7198d718d2ad63b0610492159c68088f10
Mervatk <39992888+Mervatk@users.noreply.github.com>
2018-06-07 00:29:35 +0300

    GUI Updates
    Start, pause and stop buttons functionality :)
    Graphs layout with Plus...

--- Understand_Your_Vehicle/app/src/main/java/com/example/mervatkhoury/understand_your_vehicle/AppGUI.java
+++ Understand_Your_Vehicle/app/src/main/java/com/example/mervatkhoury/understand_your_vehicle/AppGUI.java
@@ -1,6 +1,7 @@
 package com.example.mervatkhoury.understand_your_vehicle;
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,24 +12,74 @@ import com.example.mervatkhoury.understand_your_vehicle.R;
 public class AppGUI extends AppCompatActivity {
 
     ImageView add;
+    ImageView start;
+    ImageView stop;
+    ImageView pause;
+    ImageView refresh;
+    ImageView load;
+    ImageView share;
+    ImageView delete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_gui);
 
+        add = (ImageView) findViewById(R.id.add);
+        start = (ImageView) findViewById(R.id.start);
+        stop = (ImageView) findViewById(R.id.stop);
+        pause = (ImageView) findViewById(R.id.pause);
+        refresh = (ImageView) findViewById(R.id.refresh);
+        load = (ImageView) findViewById(R.id.load);
+        share = (ImageView) findViewById(R.id.share);
+        delete = (ImageView) findViewById(R.id.delete);
+
+        start.setVisibility(View.VISIBLE);
+        pause.setVisibility(View.GONE);
+        stop.setVisibility(View.GONE);
+
         addListenerOnButton();
     }
 
     public void addListenerOnButton() {
 
-        add = (ImageView) findViewById(R.id.appCompatImageButton3);
         add.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View arg0) {
                 Intent i = new Intent(getApplicationContext(),com.example.mervatkhoury.understand_your_vehicle.AppGUI.class);
                 startActivity(i);
+            }
+
+        });
+
+        start.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                stop.setVisibility(View.VISIBLE);
+                pause.setVisibility(View.VISIBLE);
+            }
+
+        });
+
+        pause.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                start.setVisibility(View.VISIBLE);
+                pause.setVisibility(View.GONE);
+            }
+
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                start.setVisibility(View.VISIBLE);
+                pause.setVisibility(View.GONE);
+                stop.setVisibility(View.GONE);
             }
 
         });
