46d909f339c664ea8c80c863934fc4b2faac0e0b
Mervatk <39992888+Mervatk@users.noreply.github.com>
2018-06-07 03:34:45 +0300

    GUI Updates
    Settings Button

--- Understand_Your_Vehicle/app/src/main/java/com/example/mervatkhoury/understand_your_vehicle/AppGUI.java
+++ Understand_Your_Vehicle/app/src/main/java/com/example/mervatkhoury/understand_your_vehicle/AppGUI.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.mervatkhoury.understand_your_vehicle.R;
 
@@ -19,6 +20,15 @@ public class AppGUI extends AppCompatActivity {
     ImageView load;
     ImageView share;
     ImageView delete;
+    ImageView setting;
+    ImageView settings_bg1;
+    ImageView settings_bg2;
+    ImageView settings_ok;
+    TextView s1;
+    TextView s2;
+    TextView s3;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +43,24 @@ public class AppGUI extends AppCompatActivity {
         load = (ImageView) findViewById(R.id.load);
         share = (ImageView) findViewById(R.id.share);
         delete = (ImageView) findViewById(R.id.delete);
+        setting = (ImageView) findViewById(R.id.settings);
+        settings_bg1 = (ImageView) findViewById(R.id.settingsBar);
+        settings_bg2 = (ImageView) findViewById(R.id.settingsBar2);
+        settings_ok = (ImageView) findViewById(R.id.ok);
+
+        s1 = (TextView) findViewById(R.id.s1);
+        s2 = (TextView) findViewById(R.id.s2);
+        s3 = (TextView) findViewById(R.id.s3);
 
         start.setVisibility(View.VISIBLE);
         pause.setVisibility(View.GONE);
         stop.setVisibility(View.GONE);
+        settings_bg1.setVisibility(View.GONE);
+        settings_bg2.setVisibility(View.GONE);
+        settings_ok.setVisibility(View.GONE);
+        s1.setVisibility(View.GONE);
+        s2.setVisibility(View.GONE);
+        s3.setVisibility(View.GONE);
 
         addListenerOnButton();
     }
@@ -49,6 +73,37 @@ public class AppGUI extends AppCompatActivity {
             public void onClick(View arg0) {
                 Intent i = new Intent(getApplicationContext(),com.example.mervatkhoury.understand_your_vehicle.AppGUI.class);
                 startActivity(i);
+            }
+
+        });
+
+        setting.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                settings_bg1.setVisibility(View.VISIBLE);
+                settings_bg2.setVisibility(View.VISIBLE);
+                settings_ok.setVisibility(View.VISIBLE);
+
+                s1.setVisibility(View.VISIBLE);
+                s2.setVisibility(View.VISIBLE);
+                s3.setVisibility(View.VISIBLE);
+            }
+
+        });
+
+        settings_ok.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                settings_bg1.setVisibility(View.GONE);
+                settings_bg2.setVisibility(View.GONE);
+                settings_ok.setVisibility(View.GONE);
+
+                s1.setVisibility(View.GONE);
+                s2.setVisibility(View.GONE);
+                s3.setVisibility(View.GONE);
+
             }
 
         });
